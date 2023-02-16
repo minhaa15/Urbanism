a0ad66bddc922375000c8daabb6671ab2cff125c
PKXLSI <122641859+PKXLSI@users.noreply.github.com>
2023-02-16 16:32:22 -0500

    rectangles

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -1,9 +1,13 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.Random;
+import java.util.ArrayList;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
@@ -16,7 +20,10 @@ public class DotGen {
     private final int square_size = 20;
 
     public Mesh generate() {
-        Set<Vertex> vertices = new HashSet<>();
+//        Set<Vertex> vertices = new HashSet<>();
+
+        ArrayList <Vertex> vertices = new ArrayList<>();
+
         // Create all the vertices
         for(int x = 0; x < width; x += square_size) {
             for(int y = 0; y < height; y += square_size) {
@@ -27,8 +34,11 @@ public class DotGen {
             }
         }
         // Distribute colors randomly. Vertices are immutable, need to enrich them
-        Set<Vertex> verticesWithColors = new HashSet<>();
+//        Set<Vertex> verticesWithColors = new HashSet<>();
         Random bag = new Random();
+
+        ArrayList <Vertex> vwc = new ArrayList<>();
+
         for(Vertex v: vertices){
             int red = bag.nextInt(255);
             int green = bag.nextInt(255);
@@ -36,10 +46,74 @@ public class DotGen {
             String colorCode = red + "," + green + "," + blue;
             Property color = Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
             Vertex colored = Vertex.newBuilder(v).addProperties(color).build();
-            verticesWithColors.add(colored);
+            vwc.add(colored);
         }
 
-        return Mesh.newBuilder().addAllVertices(verticesWithColors).build();
+        ArrayList <Vertex> verticesWithEverything = new ArrayList<>();
+
+        for(int i = 0; i < vwc.size(); i++){
+            if(i + 26 < vwc.size()){
+                Color c1 = extractColor(vwc.get(i).getPropertiesList());
+                Color c2 = extractColor(vwc.get(i+26).getPropertiesList());
+
+                int [][] c = {
+                        {c1.getRed(),c1.getGreen(), c1.getGreen()},
+                        {c2.getRed(),c2.getGreen(), c2.getGreen()}
+                };
+
+                int [] average = new int [3];
+
+                for (int j = 0; j < average.length; j++){
+                    average[j] = (int)((c[0][j]+c[1][j])/2.0);
+                }
+
+                String colorCode = average[0] + "," + average[1] + "," + average[2];
+                System.out.println("COLOR RIGHT: " + colorCode);
+                Property color = Property.newBuilder().setKey("color_right").setValue(colorCode).build();
+                Vertex colored = Vertex.newBuilder(vwc.get(i)).addProperties(color).build();
+                verticesWithEverything.add(colored);
+            }
+
+            if(i + 1 < 26){
+                Color c1 = extractColor(vwc.get(i).getPropertiesList());
+                Color c2 = extractColor(vwc.get(i+1).getPropertiesList());
+
+                int [][] c = {
+                        {c1.getRed(),c1.getGreen(), c1.getGreen()},
+                        {c2.getRed(),c2.getGreen(), c2.getGreen()}
+                };
+
+                int [] average = new int [3];
+
+                for (int j = 0; j < average.length; j++){
+                    average[j] = (int)((c[0][j]+c[1][j])/2.0);
+                }
+
+                String colorCode = average[0] + "," + average[1] + "," + average[2];
+                System.out.println("COLOR DOWN: " + colorCode);
+                Property color = Property.newBuilder().setKey("color_down").setValue(colorCode).build();
+                Vertex colored = Vertex.newBuilder(vwc.get(i)).addProperties(color).build();
+                verticesWithEverything.add(colored);
+            }
+        }
+
+        return Mesh.newBuilder().addAllVertices(verticesWithEverything).build();
     }
 
+    private Color extractColor(List<Property> properties) {
+        String val = null;
+        for(Property p: properties) {
+            if (p.getKey().equals("rgb_color")) {
+//                System.out.println(p.getValue());
+                val = p.getValue();
+            }
+        }
+        if (val == null)
+            return Color.BLACK;
+        String[] raw = val.split(",");
+        int red = Integer.parseInt(raw[0]);
+        int green = Integer.parseInt(raw[1]);
+        int blue = Integer.parseInt(raw[2]);
+        return new Color(red, green, blue);
+    }
 }
--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -9,6 +9,7 @@ import java.awt.Stroke;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import java.util.List;
 
 public class GraphicRenderer {
@@ -24,16 +25,24 @@ public class GraphicRenderer {
             Color old = canvas.getColor();
             canvas.setColor(extractColor(v.getPropertiesList()));
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, THICKNESS, THICKNESS);
+            Rectangle2D lineRight = new Rectangle2D.Double(centre_x + THICKNESS, centre_y + 1, 20-THICKNESS, 1);
+            Rectangle2D lineDown = new Rectangle2D.Double(centre_x + 1, centre_y + THICKNESS, 1, 20-THICKNESS);
             canvas.fill(point);
+
+            canvas.setColor(extractLineRight(v.getPropertiesList()));
+            canvas.fill(lineRight);
+
+            canvas.setColor(extractLineDown(v.getPropertiesList()));
+            canvas.fill(lineDown);
             canvas.setColor(old);
         }
     }
 
-    private Color extractColor(List<Property> properties) {
+    private Color extractLineRight(List<Property> properties) {
         String val = null;
         for(Property p: properties) {
-            if (p.getKey().equals("rgb_color")) {
-                System.out.println(p.getValue());
+            if (p.getKey().equals("color_right")) {
+//                System.out.println(p.getValue());
                 val = p.getValue();
             }
         }
@@ -46,4 +55,37 @@ public class GraphicRenderer {
         return new Color(red, green, blue);
     }
 
+    private Color extractLineDown(List<Property> properties) {
+        String val = null;
+        for(Property p: properties) {
+            if (p.getKey().equals("color_down")) {
+//                System.out.println(p.getValue());
+                val = p.getValue();
+            }
+        }
+        if (val == null)
+            return Color.BLACK;
+        String[] raw = val.split(",");
+        int red = Integer.parseInt(raw[0]);
+        int green = Integer.parseInt(raw[1]);
+        int blue = Integer.parseInt(raw[2]);
+        return new Color(red, green, blue);
+    }
+
+    private Color extractColor(List<Property> properties) {
+        String val = null;
+        for(Property p: properties) {
+            if (p.getKey().equals("rgb_color")) {
+//                System.out.println(p.getValue());
+                val = p.getValue();
+            }
+        }
+        if (val == null)
+            return Color.BLACK;
+        String[] raw = val.split(",");
+        int red = Integer.parseInt(raw[0]);
+        int green = Integer.parseInt(raw[1]);
+        int blue = Integer.parseInt(raw[2]);
+        return new Color(red, green, blue);
+    }
 }
