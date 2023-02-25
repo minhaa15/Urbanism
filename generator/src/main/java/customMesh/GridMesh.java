105968944262d8f2a22c4e9dbb268700dcb6de27
Yousef Shahin <yousefshahin@Yousefs-MacBook-Air.local>
2023-02-24 21:37:35 -0500

    starting the normal mesh generating method

--- generator/src/main/java/customMesh/GridMesh.java
+++ generator/src/main/java/customMesh/GridMesh.java
@@ -20,8 +20,29 @@ public class GridMesh extends MeshADT{
         presicion = p;
     }//end constructor
     @Override
-    public void generate() {   
-
-             
+    public void generate() {    
+        //Variables to help keep track of id's of items adde
+        int polygonCount = 0;
+        
+        //Figuring out height and width of the square
+        int gridSquareWidth = (int)(this.width/this.squareSize);
+        int gridSquareHeight = (int)(this.height/this.squareSize);
+        
+        //Nested for loop to traverse through Grid
+        for(double xPosition = 0; xPosition < gridSquareWidth; xPosition++){
+            for(double yPosition = 0; yPosition < gridSquareHeight; yPosition++){
+                
+                //Calculates where the top left corner of this particular square in the grid will be
+                double actualX = xPosition * squareSize;
+                double actualY = yPosition * squareSize;
+                
+                //Create a new polgon
+                MyPolygon poly = new MyPolygon(polygonCount);
+                poly.generateRandomColor();
+                this.addPolygon(poly);
+                polygonCount++;
+                
+            }//end for loop for columns
+        }//end for loop for rows
     }//end method generate
 }//end of class gridMesh
