5e918a060ab47c2d4c5d308bffc192ef56db9ae4
Yousef Shahin <yousefshahin@Yousefs-MacBook-Air.local>
2023-02-24 20:58:13 -0500

    adding GridMesh class and new MyPolygon class

---
+++ generator/src/main/java/customMesh/GridMesh.java
@@ -0,0 +1,25 @@
+package customMesh;
+
+import java.beans.VetoableChangeListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GridMesh extends MeshADT{
+    private double width;
+    private double height;
+    private double squareSize;
+  
+    public GridMesh(double width, double height, double squareSize, int p){
+        this.width = width;
+        this.height = height;
+        this.squareSize = squareSize;   
+      
+        vertices = new ArrayList<>();
+        segments = new ArrayList<>();
+        polygons = new ArrayList<>();
+        presicion = p;
+    }//end constructor
+    @Override
+    public void generate() {            
+    }//end method generate
+}//end of class gridMesh
