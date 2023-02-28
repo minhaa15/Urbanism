3ee2afd8f0803e668ba87692ba4e7a8601142475
Aman Minhas <amanrajput096.ar@gmail.com>
2023-02-27 23:00:21 -0500

    Commiting parts of f11

--- generator/src/main/java/customMesh/IrregularMesh.java
+++ generator/src/main/java/customMesh/IrregularMesh.java
@@ -63,6 +63,21 @@ public class IrregularMesh extends MeshADT{
             o1 = createVoronoi(newCoors);
             newCoors = new ArrayList<>(getCentroidsCoors(o1, coors));
         }
+        List <Coordinate> centroidList = new ArrayList<>();
+        List <Polygon> setOfPolygons = new ArrayList<>();
+
+        //creates a list of polygons based on the final voronoi diagram
+        for (int i = 0; i < o1.getNumGeometries(); i++) {
+            Geometry geo = o1.getGeometryN(i);
+            if (geo instanceof Polygon) {
+                Polygon p = (Polygon) geo;
+
+                p = (Polygon)(new ConvexHull(p.getCoordinates(), new GeometryFactory(pm)).getConvexHull());
+
+                setOfPolygons.add(p);
+                centroidList.add(new Coordinate(((Polygon) geo).getCentroid().getX(), ((Polygon) geo).getCentroid().getY()));
+            }
+        }
     }
     private List <Coordinate> createInitialPoints(){
         Random bag = new Random();
@@ -111,5 +126,90 @@ public class IrregularMesh extends MeshADT{
             //}//end for loop
         }
         return coors;
+    }
+    private void createPolygons(List <Polygon> setOfPolygons,Map<Integer, List<Integer>> neighborMap, Geometry dtbOutput){
+        int vertexIdCounter = 0;
+        int newSegmentCount = 0;
+        int polygonCount = 0;
+
+        for(Polygon p : setOfPolygons){
+            MyPolygon poly = new MyPolygon(polygonCount);
+
+            Coordinate [] thisPolygonCoordinates = p.getCoordinates();
+
+            for(Coordinate c : thisPolygonCoordinates){
+                //New Vertex with the id vertexCount
+                MyVertex vertex = new MyVertex(vertexIdCounter, this.presicion, 20);
+
+                //Set the vertex attributes
+                vertex.setXPosition(c.getX());
+                vertex.setYPosition(c.getY());
+                vertex.generateRandomColor();
+
+                //Checks if a vertex exists at this position
+                MyVertex exists = (Functions.exists(this.vertices, vertex));
+
+                //Decides whether to add a new vertex to the mesh
+                if(exists == null){
+                    this.addVertex(vertex);
+                    poly.addVertexs(vertex);
+                    vertexIdCounter++;
+                }else{
+                    poly.addVertexs(exists);
+                }
+            }
+
+            //stores a list of the vertices associated with this polygon
+            List <MyVertex> pVerts = poly.getVertexs();
+
+            //Loop to create segments
+            for(int i = 0; i < pVerts.size()-1; i++){
+                //Create a segment
+                MySegment newSegment = new MySegment(newSegmentCount, this.lineThickness);
+
+                //set segment attributes
+                newSegment.setVertex1(pVerts.get(i));
+                newSegment.setVertex2(pVerts.get(i+1));
+                newSegment.generateColor();
+
+                //Check is segment exists
+                MySegment exists = (Functions.exists(this.segments, newSegment));
+
+                //Descide whether to add new segment to mesh
+                if(exists == null){
+                    this.addSegment(newSegment);
+                    poly.addSegments(newSegment);
+                    newSegmentCount++;
+                }else{
+                    poly.addSegments(exists);
+                }
+            }
+
+            //New Vertex with the id vertexCount
+            MyVertex centroid = new MyVertex(vertexIdCounter, this.presicion, this.vertexThickness);
+
+            centroid.setXPosition(p.getCentroid().getX());
+            centroid.setYPosition(p.getCentroid().getY());
+            centroid.generateRandomColor();
+
+            //Checks if a vertex exists at this position
+            MyVertex exists = (Functions.exists(this.vertices, centroid));
+
+            //Decides whether to add a new vertex to the mesh
+            if(exists == null){
+                this.addVertex(centroid);
+                poly.setCentroidIndex(vertexIdCounter);
+                vertexIdCounter++;
+            }else{
+                poly.setCentroidIndex(exists.getId());
+            }
+
+            for(int neighbor : neighborMap.get(poly.getId())){
+                poly.addNeighbor(neighbor);
+            }
+
+            this.addPolygon(poly);
+            polygonCount++;
+        }
     }
 }
